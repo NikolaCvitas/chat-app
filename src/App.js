66d4d80b3298b6ec07a22040e1fb27af46a12522
Nikola Cvitas <ncvitas@realnetworks.com>
2021-02-02 22:57:02 +0100

    Added ChatFeed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import {ChatEngine} from 'react-chat-engine';
 
 import './App.css'
 
+import ChatFeed from './components/ChatFeed'
+
 const App = () =>{
     return (
         <ChatEngine
@@ -9,6 +11,7 @@ const App = () =>{
         projectID="c0a910eb-257b-4334-874a-abca0a0ee254"
         userName="Nikola"
         userSecret="Pokemon2020"
+        renderChatFeed={ (chatAppProps) =>  <ChatFeed { ...chatAppProps}/>}
         />
     )
 }
