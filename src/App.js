1170ee92074e89aedc0245101e0acfefd43dcae4
Nikola Cvitas <ncvitas@realnetworks.com>
2021-02-02 22:43:53 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import {ChatEngine} from 'react-chat-engine';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+const App = () =>{
+    return (
+        <ChatEngine
+        height="100vh"
+        projectID="c0a910eb-257b-4334-874a-abca0a0ee254"
+        userName="Nikola"
+        userSecret="Pokemon2020"
+        />
+    )
 }
 
 export default App;
