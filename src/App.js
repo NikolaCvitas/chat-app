9e99f04b07fa1493196cc98f746fe9bbf10bc18a
Nikola Cvitas <ncvitas@realnetworks.com>
2021-02-03 10:59:37 +0100

    LoginForm

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import {ChatEngine} from 'react-chat-engine';
 import './App.css'
 
 import ChatFeed from './components/ChatFeed'
+import LoginForm from './components/LoginForm';
 
 const App = () =>{
+
+    //if used is logged
+    if(!localStorage.getItem('username')){
+        return <LoginForm />
+    } 
     return (
         <ChatEngine
         height="100vh"
         projectID="c0a910eb-257b-4334-874a-abca0a0ee254"
         userName="Nikola"
-        userSecret="Pokemon2020"
+        userSecret="123123"
         renderChatFeed={ (chatAppProps) =>  <ChatFeed { ...chatAppProps}/>}
         />
     )
